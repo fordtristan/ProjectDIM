e3aecafdd901082f6d91b8b5372dfb2a6937a91a
Tristan Ford <Tristan Ford>
2017-04-03 21:37:26 -0400

    Tristan's initial files

---
+++ DIM/app/src/main/java/org/tristanford/dim/dim/MeaningfulActivity.java
@@ -0,0 +1,13 @@
+package org.tristanford.dim.dim;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeaningfulActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meaningful);
+    }
+}
